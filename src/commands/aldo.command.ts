7a73d0c65dcb54609de9a934d1a1df76206e0042
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-01-21 20:28:03 +0300

    add /aldo

---
+++ src/commands/aldo.command.ts
@@ -0,0 +1,37 @@
+import { SlashCommand } from '../modules/CommandManager'
+import { SlashCommandBuilder } from 'discord.js'
+import axios from 'axios'
+import { load } from 'cheerio'
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('aldo')
+        .setDescription('The wikipedia nerd')
+        .addBooleanOption(bo => bo
+            .setName('ephemeral')
+            .setDescription('Should the response show up only for you?')
+            .setRequired(false)
+        ),
+    async execute(interaction) {
+        await interaction.deferReply({
+            ephemeral: interaction.options.getBoolean('ephemeral', false) ?? undefined
+        })
+        const url = await randomUnusualArticle().catch(() => '❌ Failed to get article')
+        await interaction.editReply(url)
+    },
+} satisfies SlashCommand
+
+async function randomUnusualArticle(): Promise<string> {
+    const url = 'https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles'
+    const res = await axios.get(url)
+    const $ = load(res.data)
+    const articleLinks: string[] = []
+    $('div.mw-parser-output ul li a').each((index, element) => {
+        const href = $(element).attr('href')
+        if (href && href.startsWith('/wiki/') && !href.includes(':')) {
+            articleLinks.push(`https://en.wikipedia.org${href}`)
+        }
+    })
+    const randomLink = articleLinks[Math.floor(Math.random() * articleLinks.length)]
+    return randomLink
+}
