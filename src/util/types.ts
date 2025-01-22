9f3e15eed290bbd4b154c13567b9f6d1d2a5610c
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-01-22 21:23:01 +0300

    types: introduce HexColor type and update ColorDefinition to use it

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,14 +1,14 @@
-import { Guild, BaseInteraction, GuildChannel, Message, GuildMember, User, CommandInteraction, ChatInputCommandInteraction, type APIInteractionDataResolvedChannel, Client } from 'discord.js'
+import {
+    Guild, BaseInteraction, GuildChannel, Message, GuildMember, CommandInteraction,
+    ChatInputCommandInteraction, type APIInteractionDataResolvedChannel, Client, User
+} from 'discord.js'
 
 export type GuildIdResolvable = string | Guild | BaseInteraction | GuildChannel | Message
 export type UserIdResolvable = GuildMember | User | string | Message
-export type ChannelIdResolvable = GuildChannel | Message | CommandInteraction | ChatInputCommandInteraction | string | APIInteractionDataResolvedChannel
-
-/**
- * Provide at least one of the properties of the object
- */
+export type ChannelIdResolvable = GuildChannel | Message | CommandInteraction |
+    ChatInputCommandInteraction | string | APIInteractionDataResolvedChannel
 export type AtleastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
-
 export interface DiscordEventListener {
     default: (client: Client) => void
 }
+export type HexColor = `#${string}`
