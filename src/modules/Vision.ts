51c1ce9339dd0c8511a95aec70c1e1fe3248ed2d
emberglaze <emberglaze@emberglaze.ru>
2025-01-28 00:19:10 +0300

    vision: add logging

--- src/modules/Vision.ts
+++ src/modules/Vision.ts
@@ -1,4 +1,6 @@
 import { Client } from '@gradio/client'
+import { Logger } from '../util/logger'
+const logger = Logger.new('Vision')
 
 export default class Vision {
     private static instance: Vision
@@ -11,17 +13,22 @@ export default class Vision {
         return Vision.instance
     }
     public async init() {
+        logger.info('Initializing Gradio client')
         this.GradioClient = await Client.connect('KingNish/Qwen2-VL-7B')
+        logger.ok('Gradio client initialized')
     }
     public async captionImage(imageUrl: string): Promise<string> {
         if (!this.GradioClient) {
             throw new Error('Gradio client not initialized')
         }
+        logger.info('Fetching image')
         const image = await (await fetch(imageUrl)).blob()
+        logger.info('Predicting image caption')
         const response = await this.GradioClient.predict('/qwen_inference', {
             media_input: image,
             text_input: 'Describe this image in detail'
         })
+        logger.ok(`Image caption predicted: ${response.data}`)
         return response.data as string
     }
 }
