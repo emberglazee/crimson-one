63b6ce7b0899b4781b60d7e76b0ec457221a2cc5
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-03-20 23:53:43 +0300

    add /morse

---
+++ src/commands/morse.command.ts
@@ -0,0 +1,48 @@
+import { SlashCommandBuilder } from 'discord.js'
+import type { SlashCommand } from '../modules/CommandManager'
+import { Logger } from '../util/logger'
+const logger = new Logger('/morse')
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('morse')
+        .setDescription('Decode or encode text from or to morse code')
+        .addSubcommand(sc => sc
+            .setName('decode')
+            .setDescription('Decode morse code into text')
+            .addStringOption(so => so
+                .setName('code')
+                .setDescription('Morse code to attempt to decode')
+            )
+        ).addSubcommand(sc => sc
+            .setName('encode')
+            .setDescription('Encode text into morse code')
+            .addStringOption(so => so
+                .setName('text')
+                .setDescription('text to encode into morse code')
+            )
+        ),
+    async execute(interaction) {
+        try {
+            const subcommand = interaction.options.getSubcommand(true)
+            const morse = await import('morse')
+            let code, text = ''
+            switch (subcommand) {
+                case 'decode':
+                    code = interaction.options.getString('code', true)
+                    text = morse.decode(code)
+                    await interaction.reply(text)
+                    return
+                case 'encode':
+                    text = interaction.options.getString('text', true)
+                    code = morse.encode(text)
+                    await interaction.reply(code)
+                    return
+            }
+        } catch (e) {
+            const error = e as Error
+            logger.error(error.stack ?? error.message ?? error)
+            await interaction.reply(`<@341123308844220447> something went wrong with the morse command -> \`${error.message ?? error}\`\n-# check the full error stack in the console, nerd`)
+        }
+    }
+} satisfies SlashCommand
