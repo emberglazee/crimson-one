2947c3650c0f73214d1abc85d27a9aa09e60e85f
emberglaze <emberglaze@emberglaze.ru>
2025-03-24 08:39:44 +0300

    screamonsight: invisible

--- src/modules/ScreamOnSight.ts
+++ src/modules/ScreamOnSight.ts
@@ -14,12 +14,22 @@ export class ScreamOnSight {
             pattern: [/ronald mcdonald/gmi],
             async action(message) {
                 const variants = [
-                    'https://cdn.discordapp.com/attachments/311334325402599425/1307805230246793287/HjDvLkbP0eP6oLok.mov?ex=67e1c108&is=67e06f88&hm=69438eeed54ba9ee768fa615607b7f256260d0055b970f2824eff9795106e01a&',
-                    'https://cdn.discordapp.com/attachments/1125900471924699178/1303877939049402409/cachedVideo.mov?ex=67e1f7f5&is=67e0a675&hm=108fde1dc8376d2db90d81300944d2e232d9fdecb3ea0bbc139567bb2473233a&',
-                    'https://cdn.discordapp.com/attachments/1335990015989125213/1337635592976076831/areyoureadyforzenewworldorder.mp4?ex=67e22bb9&is=67e0da39&hm=738de76475fd36f40dcecd80db555b2507bb460828a422d038fdbdc60a57dbb8&',
-                    'https://media.discordapp.net/attachments/1267488539503886386/1346032804449882172/lv_0_20250302125127.mp4?ex=67e1bcfc&is=67e06b7c&hm=ba256a66f0c02d41be35bef627b7b84d1629df3e0aee8158c3b83615eadb279e&'
+                    'https://cdn.discordapp.com/attachments/1125900471924699178/1303877939049402409/cachedVideo.mov?ex=67e1f7f5&is=67e0a675&hm=108fde1dc8376d2db90d81300944d2e232d9fdecb3ea0bbc139567bb2473233a&', // Q2
+                    'https://media.discordapp.net/attachments/1267488539503886386/1346032804449882172/lv_0_20250302125127.mp4?ex=67e1bcfc&is=67e06b7c&hm=ba256a66f0c02d41be35bef627b7b84d1629df3e0aee8158c3b83615eadb279e&' // Q4
                 ]
-                const variant = variants[randRange(1, 4)]
+                const variant = variants[randRange(1, 2)]
+                await message.reply(variant)
+            },
+        },
+        {
+            pattern: [/invisible/gmi],
+            async action(message) {
+                const variants = [
+                    'https://tenor.com/view/mgs-metal-gear-solid-phantom-pain-venom-snake-gif-5631901306578330322',
+                    'https://tenor.com/view/venom-snake-walk-mgsv-mgs-mgs5-gif-27690753',
+                    'https://tenor.com/view/metal-gear-venom-snake-gif-26285931'
+                ]
+                const variant = variants[randRange(1, 3)]
                 await message.reply(variant)
             },
         }
