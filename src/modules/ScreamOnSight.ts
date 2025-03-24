479389b08b3ce43ea48560daceaad3ef2d944107
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-03-24 09:09:56 +0300

    screamonsight: spade incoming

--- src/modules/ScreamOnSight.ts
+++ src/modules/ScreamOnSight.ts
@@ -1,6 +1,6 @@
 import type { Message } from 'discord.js'
 import type { ScreamOnSightTrigger } from '../types/types'
-import { randRange } from '../util/functions'
+import { randArr } from '../util/functions'
 
 export class ScreamOnSight {
     triggers: ScreamOnSightTrigger[] = [
@@ -17,7 +17,7 @@ export class ScreamOnSight {
                     'https://cdn.discordapp.com/attachments/1125900471924699178/1303877939049402409/cachedVideo.mov?ex=67e1f7f5&is=67e0a675&hm=108fde1dc8376d2db90d81300944d2e232d9fdecb3ea0bbc139567bb2473233a&', // Q2
                     'https://media.discordapp.net/attachments/1267488539503886386/1346032804449882172/lv_0_20250302125127.mp4?ex=67e1bcfc&is=67e06b7c&hm=ba256a66f0c02d41be35bef627b7b84d1629df3e0aee8158c3b83615eadb279e&' // Q4
                 ]
-                const variant = variants[randRange(1, variants.length) - 1]
+                const variant = randArr(variants)
                 await message.reply(variant)
             },
         },
@@ -30,7 +30,7 @@ export class ScreamOnSight {
                     'https://tenor.com/view/metal-gear-venom-snake-gif-26285931',
                     'https://tenor.com/view/metal-gear-solid-snake-big-boss-gif-12248663'
                 ]
-                const variant = variants[randRange(1, variants.length) - 1]
+                const variant = randArr(variants)
                 await message.reply(variant)
             },
         },
@@ -55,7 +55,22 @@ export class ScreamOnSight {
                     'https://tenor.com/view/scary-scream-aaaa-martin-scorsese-scorsese-gif-1034167448643503493',
                     'https://tenor.com/view/absolute-cinema-absolute-freaky-gif-479618626165428928'
                 ]
-                const variant = variants[randRange(1, variants.length) - 1]
+                const variant = randArr(variants)
+                await message.reply(variant)
+            }
+        },
+        {
+            pattern: [/spade incoming/gmi],
+            async action(message) {
+                const variants = [
+                    'https://cdn.discordapp.com/attachments/1267488539503886386/1344353319849558140/39e67e09-c811-479a-8011-9fb07b917e0e.gif?ex=67e23858&is=67e0e6d8&hm=79d98a161943f6e42ac43a9cf12b72000ea72102f43c8f936a5930ce735ab5ba&',
+                    'https://cdn.discordapp.com/attachments/1225579254448652420/1280117275063484458/makesweet-9h1bj4.gif?ex=67e1e15d&is=67e08fdd&hm=bf1121e8169df5acf545947cf577ca81d2e486db04bec7d2d77686f27909f28b&',
+                    'https://tenor.com/view/shots-fired-smoke-shoot-gif-15830209',
+                    'https://tenor.com/view/dog-swing-gif-23878746',
+                    'https://tenor.com/view/grand-theft-auto-gta-gta5-gta-v-davey-gif-25947802',
+                    'https://tenor.com/view/project-wingman-crimson-1-cordium-consequence-of-power-gif-18137013603651714218'
+                ]
+                const variant = randArr(variants)
                 await message.reply(variant)
             },
         }
