5d60d5018564b5b078bf3293acd52261756f01c8
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-03-24 22:27:56 +0300

    screamonsight: code refactor

--- src/modules/ScreamOnSight.ts
+++ src/modules/ScreamOnSight.ts
@@ -13,33 +13,29 @@ export class ScreamOnSight {
         {
             pattern: [/ronald mcdonald/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     'https://cdn.discordapp.com/attachments/1125900471924699178/1303877939049402409/cachedVideo.mov?ex=67e1f7f5&is=67e0a675&hm=108fde1dc8376d2db90d81300944d2e232d9fdecb3ea0bbc139567bb2473233a&', // Q2
                     'https://media.discordapp.net/attachments/1267488539503886386/1346032804449882172/lv_0_20250302125127.mp4?ex=67e1bcfc&is=67e06b7c&hm=ba256a66f0c02d41be35bef627b7b84d1629df3e0aee8158c3b83615eadb279e&' // Q4
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                ]))
             },
         },
         {
             pattern: [/invisible/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     'https://tenor.com/view/mgs-metal-gear-solid-phantom-pain-venom-snake-gif-5631901306578330322',
                     'https://tenor.com/view/venom-snake-walk-mgsv-mgs-mgs5-gif-27690753',
                     'https://tenor.com/view/metal-gear-venom-snake-gif-26285931',
                     'https://tenor.com/view/metal-gear-solid-snake-big-boss-gif-12248663',
                     'https://tenor.com/view/metal-gear-solid-phantom-pain-metal-gear-solid-v-snake-big-boss-gif-6526414909388443363',
                     'https://tenor.com/view/mgs-mgsv-metal-gear-solid-big-boss-gif-27478240'
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                ]))
             },
         },
         {
             pattern: [/absolute cinema/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     'https://tenor.com/view/absolute-cinema-cinema-cine-absolute-cine-gif-5324030207930286506',
                     'https://tenor.com/view/johnqt-cinema-johnqt-absolute-cinema-gif-8595958281962294369',
                     'https://tenor.com/view/me-atrapaste-es-cine-its-cinema-cinema-esto-es-cine-gif-17729711691959966457',
@@ -62,15 +58,13 @@ export class ScreamOnSight {
                     'https://tenor.com/view/absolute-cinema-ryo-yamada-ry-ryo-meme-gif-9636831842365625847',
                     'https://tenor.com/view/absolute-cinema-absolute-cinema-boykisser-gif-8548693639845499838',
                     'https://tenor.com/view/mattis-ai-generated-absolute-cinema-black-and-white-greyscale-gif-15545318427947589245'
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                ]))
             }
         },
         {
             pattern: [/spade incoming/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     'https://cdn.discordapp.com/attachments/1267488539503886386/1344353319849558140/39e67e09-c811-479a-8011-9fb07b917e0e.gif?ex=67e23858&is=67e0e6d8&hm=79d98a161943f6e42ac43a9cf12b72000ea72102f43c8f936a5930ce735ab5ba&',
                     'https://cdn.discordapp.com/attachments/1225579254448652420/1280117275063484458/makesweet-9h1bj4.gif?ex=67e1e15d&is=67e08fdd&hm=bf1121e8169df5acf545947cf577ca81d2e486db04bec7d2d77686f27909f28b&',
                     'https://tenor.com/view/shots-fired-smoke-shoot-gif-15830209',
@@ -78,15 +72,13 @@ export class ScreamOnSight {
                     'https://tenor.com/view/grand-theft-auto-gta-gta5-gta-v-davey-gif-25947802',
                     'https://tenor.com/view/project-wingman-crimson-1-cordium-consequence-of-power-gif-18137013603651714218',
                     'https://tenor.com/view/%D0%BF%D1%80%D0%B0%D1%86%D1%8E%D1%94-%D0%BF%D0%BF%D0%BE-%D0%BF%D0%BF%D0%BE-%D0%BF%D0%B2%D0%BE-%D0%BF%D0%B5%D1%82%D1%80%D1%96%D0%BE%D1%82-%D0%BF%D0%B0%D1%82%D1%80%D1%96%D0%BE%D1%82-gif-12934051785885241735'
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                ]))
             },
         },
         {
             pattern: [/embi/gmi, /\bember/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     '<@341123308844220447> mentioned',
                     '<@341123308844220447> yo they yapping abt you or smth',
                     'is that a fucking <@341123308844220447> reference!?',
@@ -97,9 +89,7 @@ export class ScreamOnSight {
                     '<@341123308844220447> https://tenor.com/view/cat-silly-gif-yellow-cat-gif-16281670106766812594',
                     '<@341123308844220447> добрый вечер я диспетчер чекни что за пиздёшь тут происходит',
                     '<@341123308844220447> https://tenor.com/view/cat-grab-project-wingman-eminent-domain-project-wingman-cascadia-captain-woodward-gif-8486343655825859387'
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                ]))
             },
         },
         {
@@ -111,14 +101,15 @@ export class ScreamOnSight {
         {
             pattern: [/\bash\b/gmi, /ashleigh/gmi],
             async action(message) {
-                const variants = [
+                await message.reply(randArr([
                     'https://cdn.discordapp.com/attachments/1267488539503886386/1331344509036003338/file.jpg?ex=67e25af2&is=67e10972&hm=847306a43bf42323c2ffa3e1b641d1a4bd1c3a737d89a526e84df7e034694dc0&',
                     'https://media.discordapp.net/attachments/1351770874625130577/1351770895559164004/image.png?ex=67e2d680&is=67e18500&hm=235591676beae2c24528840dc66a2b78ee4fbef5fb16747edb0783b8ef561cb3&format=webp&quality=lossless&',
                     'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/segsymg2.png',
-                    'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/ddwyn9pr.png'
-                ]
-                const variant = randArr(variants)
-                await message.reply(variant)
+                    'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/ddwyn9pr.png',
+                    'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/97ggmkis.png',
+                    'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/xp4356ma.png',
+                    'https://r2.e-z.host/553257c7-6ffa-45c8-9d1c-531ea7d264db/1g2x2058.png'
+                ]))
             },
         }
     ]
