79ec49975891d4e371dd0479d1779e413cbe4b95
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-03-24 07:56:58 +0300

    screamonsight: can use both regex and string as the trigger pattern

--- src/modules/ScreamOnSight.ts
+++ src/modules/ScreamOnSight.ts
@@ -4,13 +4,13 @@ import type { ScreamOnSightTrigger } from '../types/types'
 export class ScreamOnSight {
     triggers: ScreamOnSightTrigger[] = [
         {
-            regex: [/comic/gm, /peg/gm],
+            pattern: ['comic', 'peg'],
             async action(message) {
                 await message.reply('<:peg:1341742361004212285><:ging:1341742389257310279>')
             }
         }
     ]
     async processMessage(message: Message) {
-        for (const { regex, action } of this.triggers) if (regex.some(r => r.test(message.content))) await action(message)
+        for (const { pattern, action } of this.triggers) if (pattern.some(r => r instanceof RegExp ? r.test(message.content) : message.content.includes(r))) await action(message)
     }
 }
