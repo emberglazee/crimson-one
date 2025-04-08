2987a84d4243c5ec4057850bc6edec4dc9b89aa7
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-04-08 11:58:29 +0300

    long live neurocrimson 1

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,7 +1,6 @@
 import { Client } from 'discord.js'
 import { Logger } from '../util/logger'
 import CommandHandler from '../modules/CommandManager'
-import CrimsonChat from '../modules/CrimsonChat'
 import chalk from 'chalk'
 const logger = Logger.new('event.interactionCreate')
 
@@ -11,15 +10,6 @@ export default function onInteractionCreate(client: Client) {
         if (!interaction.isChatInputCommand() && !interaction.isUserContextMenuCommand() && !interaction.isMessageContextMenuCommand()) {
             if (interaction.isRepliable()) await interaction.reply(`⚠️ Unhandled interaction type ${interaction.type}`)
             return
-        }
-
-        const isMainChannel = interaction.channel?.id === '1335992675459141632'
-        const isTestingServer = interaction.guildId === '1335971145014579263'
-
-        // Track command usage in CrimsonChat channel if it's a slash command
-        if (interaction.isChatInputCommand() && (isMainChannel || isTestingServer)) {
-            const crimsonChat = CrimsonChat.getInstance()
-            await crimsonChat.trackCommandUsage(interaction)
         }
 
         commandHandler.handleInteraction(interaction).catch(err => {
