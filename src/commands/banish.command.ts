9c7d409a396bf11efd9c981824211922286349d8
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-04-10 15:47:22 +0300

    /banish: add check if target already has banished role

--- src/commands/banish.command.ts
+++ src/commands/banish.command.ts
@@ -16,6 +16,7 @@ export default {
             await reply(`❌ why is \`interaction.guild\` nonexistant i thought i set the interaction context type to guilds only wtf ${PING_EMBERGLAZE}`)
             return
         }
+
         const member = interaction.member as GuildMember | null
         if (!member) {
             await reply(`❌ for some reason i cant find you as a server member *sigh* ${PING_EMBERGLAZE}`)
@@ -25,19 +26,28 @@ export default {
             await reply(`❌ ${PING_EMBERGLAZE} get over here fuckface your shitty fucking permissions check in your own shitty fucking slash command parser didnt fucking work so i stopped this fucking guy from running the fucking command (missing ManageRoles)`)
             return
         }
+
         const target = interaction.options.getUser('member', true)
         const targetMember = await interaction.guild.members.fetch(target)
         if (!targetMember) {
             await reply(`❌ ${PING_EMBERGLAZE} target member doesnt exist, FIX MEEEEEEEEE`)
             return
         }
+
         const role = await interaction.guild.roles.fetch('1331170880591757434')
         if (!role) {
             await reply(`❌ ${PING_EMBERGLAZE} banished role doesnt exist, wrong id? \`1331170880591757434\``)
             return
         }
+
+        const roles = targetMember.roles.cache.filter(role => role.name !== '@everyone')
+        if (roles.find(r => r.id === role.id)) {
+            await reply(`⚠️ they already have a banished role`)
+            return
+        }
         await targetMember.roles.add(role)
         await reply(`Banished ${targetMember} for anti-regime behavior`)
+
         const banishedChannel = await interaction.guild.channels.fetch('1331173298528321587')
         if (!banishedChannel) {
             await followUp(`⚠️ cant find the banished channel \`1331173298528321587\`, whatever, you cook bro`)
