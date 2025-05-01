777a56db5993d6d5c314071c21c3b6cf04d9cf7a
emberglaze <emberglaze@emberglaze.ru>
2025-05-01 20:08:27 +0300

    add /unbanish

---
+++ src/commands/unbanish.command.ts
@@ -0,0 +1,63 @@
+import { ChannelType, GuildMember, InteractionContextType, SlashCommandBuilder } from 'discord.js'
+import type { GuildSlashCommand } from '../modules/CommandManager'
+import { PING_EMBERGLAZE } from '../util/constants'
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('unbanish')
+        .setDescription('Remove the `banished` role from a server member')
+        .addUserOption(uo => uo
+            .setName('member')
+            .setDescription('Server member to unbanish')
+            .setRequired(true)
+        ).setContexts(InteractionContextType.Guild),
+    async execute(interaction, { reply, followUp }) {
+        if (!interaction.guild) {
+            await reply(`❌ why is \`interaction.guild\` nonexistant i thought i set the interaction context type to guilds only wtf ${PING_EMBERGLAZE}`)
+            return
+        }
+
+        const member = interaction.member as GuildMember | null
+        if (!member) {
+            await reply(`❌ for some reason i cant find you as a server member *sigh* ${PING_EMBERGLAZE}`)
+            return
+        }
+        if (!member.permissions.has('ManageRoles')) {
+            await reply(`❌ ${PING_EMBERGLAZE} get over here fuckface your shitty fucking permissions check in your own shitty fucking slash command parser didnt fucking work so i stopped this fucking guy from running the fucking command (missing ManageRoles)`)
+            return
+        }
+
+        const target = interaction.options.getUser('member', true)
+        const targetMember = await interaction.guild.members.fetch(target)
+        if (!targetMember) {
+            await reply(`❌ ${PING_EMBERGLAZE} target member doesnt exist, FIX MEEEEEEEEE`)
+            return
+        }
+
+        const role = await interaction.guild.roles.fetch('1331170880591757434')
+        if (!role) {
+            await reply(`❌ ${PING_EMBERGLAZE} banished role doesnt exist, wrong id? \`1331170880591757434\``)
+            return
+        }
+
+        const roles = targetMember.roles.cache.filter(role => role.name !== '@everyone')
+        if (!roles.find(r => r.id === role.id)) {
+            await reply(`⚠️ they don't have the banished role`)
+            return
+        }
+        await targetMember.roles.remove(role)
+        await reply(`Unbanished ${targetMember} for good behavior`)
+
+        const generalChannel = await interaction.guild.channels.fetch('1267488539503886386')
+        if (!generalChannel) {
+            await followUp(`⚠️ cant find the general channel \`1267488539503886386\`, whatever, you cook bro`)
+            return
+        }
+        if (generalChannel.type !== ChannelType.GuildText) {
+            await followUp(`⚠️ general channel is not guild text, k i guess (\`1267488539503886386\`)`)
+            return
+        }
+        await generalChannel.send(`${targetMember} has been unbanished for good behavior, welcome back!`)
+    },
+    guildId: '958518067690868796'
+} satisfies GuildSlashCommand
