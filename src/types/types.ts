52bc821c22f895867c0bbf49300469a61e69d9f0
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2025-05-03 20:24:22 +0300

    types: remove unused crimsonchat types, add jsdoc comments

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,9 @@
 import {
     Guild, BaseInteraction, GuildChannel, Message, GuildMember, CommandInteraction,
-    ChatInputCommandInteraction, type APIInteractionDataResolvedChannel, Client, User,
-    TextChannel
+    ChatInputCommandInteraction, type APIInteractionDataResolvedChannel, Client, User
 } from 'discord.js'
 import { EMBERGLAZE_ID, PING_EMBERGLAZE } from '../util/constants'
+
 // Command Manager Types
 import {
     SlashCommandBuilder,
@@ -16,15 +16,46 @@ import {
     type PermissionsString
 } from 'discord.js'
 
+/**
+ * Guild ID Resolvable
+ * @param {string | Guild | BaseInteraction | GuildChannel | Message} id - The ID for the guild
+ */
 export type GuildIdResolvable = string | Guild | BaseInteraction | GuildChannel | Message
+
+/**
+ * User ID Resolvable
+ * @param {GuildMember | User | string | Message} id - The ID for the user
+ */
 export type UserIdResolvable = GuildMember | User | string | Message
+
+/**
+ * Channel ID Resolvable
+ * @param {GuildChannel | Message | CommandInteraction | ChatInputCommandInteraction | string | APIInteractionDataResolvedChannel} id - The ID for the channel
+ */
 export type ChannelIdResolvable = GuildChannel | Message | CommandInteraction |
     ChatInputCommandInteraction | string | APIInteractionDataResolvedChannel
+
+/**
+ * At least One
+ * @param {T} T - The type for the at least one
+ * @param {U} U - The type for the at least one
+ */
 export type AtleastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
+
+/**
+ * Discord Event Listener
+ * @param {Client} client - The client for the event listener
+ */
 export interface DiscordEventListener {
     default: (client: Client) => void
 }
+
+/**
+ * Hex Color
+ * @param {string} color - The color for the hex color
+ */
 export type HexColor = `#${string}`
+
 export interface Emojis {
     billy: Emoji[]
 }
@@ -47,100 +78,6 @@ export interface GitHubPushEvent {
     head_commit: GitHubCommit
 }
 
-// CrimsonChat types
-export interface UserMessageOptions {
-    username: string
-    displayName: string
-    serverDisplayName: string
-    respondingTo?: {
-        targetUsername: string
-        targetText: string
-    }
-    imageAttachments?: string[]
-    contextMessages?: Array<{
-        content: string
-        username: string
-        displayName: string
-        serverDisplayName: string
-        guildName?: string
-        channelName?: string
-    }>
-    targetChannel?: TextChannel
-    guildName?: string
-    channelName?: string
-}
-
-export interface UserStatus {
-    roles: string[]
-    presence: {
-        name: string
-        type: number
-        state?: string
-        details?: string
-        createdAt: string
-    }[] | 'offline or no activities'
-}
-
-export interface MentionData {
-    type: 'mention'
-    id: string
-    username: string
-}
-
-export interface FormattedUserMessage {
-    username: string
-    displayName: string
-    serverDisplayName: string
-    currentTime: string
-    text: string
-    mentions?: MentionData[]
-    attachments?: string[]
-    respondingTo?: {
-        targetUsername: string
-        targetText: string
-    }
-    userStatus: UserStatus | 'unknown'
-}
-
-export interface ProcessedCommand {
-    content: string | null
-    hadCommands: boolean
-}
-
-export interface UserPresenceInfo {
-    roles: string[]
-    presence: {
-        name: string
-        type: number
-        state?: string
-        details?: string
-        createdAt: string
-    }[] | 'offline or no activities'
-}
-
-export interface Memory {
-    content: string
-    context?: string
-    evaluation?: string
-    timestamp: number
-    importance: 1 | 2 | 3 | 4 | 5
-}
-
-export interface DiscordEmbed {
-    title?: string
-    description?: string
-    color: number
-    fields?: { name: string; value: string }[]
-    command?: {
-        name: string
-        params?: string
-    }
-    footer?: string
-    author?: string
-}
-
-export type ChatResponse = string | { embed: DiscordEmbed } | { command: { name: string, params: string[] } }
-export type ChatResponseArray = ChatResponse[]
 
 export interface ScreamOnSightTrigger {
     pattern: Array<RegExp | string | ((message: Message) => boolean)>
@@ -155,6 +92,9 @@ export type JSONResolvable = string | number | boolean | {[key: string]: JSONRes
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ExplicitAny = any
 
+/**
+ * Slash Command Helpers, for convenience
+ */
 export type SlashCommandHelpers = {
     reply: ChatInputCommandInteraction['reply']
     deferReply: ChatInputCommandInteraction['deferReply']
@@ -166,6 +106,11 @@ export type SlashCommandHelpers = {
     pingMe: typeof PING_EMBERGLAZE
 }
 
+/**
+ * Slash Command Props
+ * @param {SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'> | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder} data - The data for the slash command
+ * @param {PermissionsBitField[]} permissions - The permissions for the slash command
+ */
 export type SlashCommandProps = {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'> | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder
     permissions?: PermissionsBitField[]
@@ -175,22 +120,49 @@ export type SlashCommandProps = {
     ) => Promise<void>
 }
 
+/**
+ * Slash Command Interface
+ * @param {SlashCommandProps} data - The data for the slash command
+ * @param {PermissionsBitField[]} permissions - The permissions for the slash command
+ */
 export interface ISlashCommand extends SlashCommandProps {}
 
+/**
+ * Slash Command Class
+ * @abstract
+ * @param {SlashCommandProps} data - The data for the slash command
+ * @param {PermissionsBitField[]} permissions - The permissions for the slash command
+ */
 export abstract class SlashCommand implements ISlashCommand {
     data!: SlashCommandProps['data']
     permissions?: SlashCommandProps['permissions']
     execute!: SlashCommandProps['execute']
 }
 
+/**
+ * Guild Slash Command Interface
+ * @param {ISlashCommand} data - The data for the slash command
+ * @param {PermissionsBitField[]} permissions - The permissions for the slash command
+ */
 export interface IGuildSlashCommand extends ISlashCommand {
     guildId: string
 }
 
+/**
+ * Guild Slash Command Class
+ * @abstract
+ * @param {ISlashCommand} data - The data for the slash command
+ * @param {PermissionsBitField[]} permissions - The permissions for the slash command
+ */
 export abstract class GuildSlashCommand extends SlashCommand implements IGuildSlashCommand {
     guildId!: string
 }
 
+/**
+ * Context Menu Command Props
+ * @param {ContextMenuCommandBuilder} data - The data for the context menu command
+ * @param {2 | 3} type - The type of the context menu command
+ */
 export type ContextMenuCommandProps<T extends 2 | 3 = 2 | 3> = {
     data: ContextMenuCommandBuilder
     type: T
@@ -201,12 +173,26 @@ export type ContextMenuCommandProps<T extends 2 | 3 = 2 | 3> = {
     permissions?: SlashCommandProps['permissions']
 }
 
+/**
+ * Context Menu Interaction Type
+ * @param {2 | 3} T - The type of the context menu command
+ */
 export type ContextMenuInteractionType<T extends 2 | 3> = T extends 2
     ? UserContextMenuCommandInteraction
     : MessageContextMenuCommandInteraction
 
+/**
+ * Context Menu Command Interface
+ * @param {ContextMenuCommandProps} data - The data for the context menu command
+ * @param {2 | 3} type - The type of the context menu command
+ */
 export interface IContextMenuCommand<T extends 2 | 3 = 2 | 3> extends ContextMenuCommandProps<T> {}
 
+/**
+ * Context Menu Command Class
+ * @param {ContextMenuCommandProps} data - The data for the context menu command
+ * @param {2 | 3} type - The type of the context menu command
+ */
 export abstract class ContextMenuCommand<T extends 2 | 3 = 2 | 3> implements IContextMenuCommand<T> {
     data!: ContextMenuCommandProps<T>['data']
     type!: ContextMenuCommandProps<T>['type']
@@ -214,12 +200,20 @@ export abstract class ContextMenuCommand<T extends 2 | 3 = 2 | 3> implements ICo
     permissions?: ContextMenuCommandProps['permissions']
 }
 
+/**
+ * Class Not Initialized Error
+ */
 export class ClassNotInitializedError extends Error {
     constructor() {
         super('Command handler has not been initialized! Call init() first')
     }
 }
 
+/**
+ * Missing Permissions Error
+ * @param {string} message - The message for the error
+ * @param {PermissionsBitField[] | PermissionsString[]} permissions - The permissions for the error
+ */
 export class MissingPermissionsError extends Error {
     permissions: PermissionsBitField[] | PermissionsString[]
     constructor(message: string, permissions: PermissionsBitField[] | PermissionsString[]) {
