940cd90f4ce94e32a01ed0125c6f8a97307a9eac
emberglaze <emberglaze@emberglaze.ru>
2025-05-08 20:22:41 +0300

    add /shapesinc; messagecreate: send image attachments to the shape

---
+++ src/commands/shapesinc.command.ts
@@ -0,0 +1,45 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { GuildSlashCommand } from '../types/types'
+import { shapesInc } from '..'
+export default {
+    data: new SlashCommandBuilder()
+        .setName('shapesinc')
+        .setDescription('Set of commands to manage the shapes.inc chat')
+        .addSubcommand(sc => sc
+            .setName('wack')
+            .setDescription('Clear chat history')
+        ).addSubcommand(sc => sc
+            .setName('changeshape')
+            .setDescription('Change the shape used by the bot')
+            .addStringOption(so => so
+                .setName('vanity')
+                .setDescription('The vanity of the shape to use')
+                .setRequired(false)
+            ).addStringOption(so => so
+                .setName('uuid')
+                .setDescription('The UUID of the shape to use')
+                .setRequired(false)
+            )
+        ),
+    async execute({ reply }, interaction) {
+        const subcommand = interaction.options.getSubcommand()
+        switch (subcommand) {
+            case 'wack':
+                await shapesInc.clearChat()
+                await reply('Chat history cleared')
+                break
+            case 'changeshape':
+                const vanity = interaction.options.getString('vanity')
+                const uuid = interaction.options.getString('uuid')
+                if (vanity) {
+                    await shapesInc.changeShapeByVanity(vanity)
+                }
+                if (uuid) {
+                    await shapesInc.changeShapeByUUID(uuid)
+                }
+                await reply(`Shape changed to ${shapesInc.shapeVanity}`)
+                break
+        }
+    },
+    guildId: '958518067690868796'
+} satisfies GuildSlashCommand
