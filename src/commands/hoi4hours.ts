844a54f4ad500088819ab49d66a9ddd8b877a30a
emberglaze <emberglaze@emberglaze.ru>
2025-05-22 21:12:11 +0300

    /hoi4hours: refactor check for hoi4 and the code around it

--- src/commands/hoi4hours.ts
+++ src/commands/hoi4hours.ts
@@ -23,27 +23,27 @@ export default {
         const data: SteamAPIResponse = await response.json()
         const games = data.response.games
         const hoi4 = games.find(game => game.appid === 394360)
-        if (hoi4) {
-            const totalHours = hoi4.playtime_forever / 60
-            const hours = totalHours.toFixed(4)
-
-            const days = Math.floor(totalHours / 24)
-            const months = Math.floor(days / 30)
-            const years = Math.floor(days / 365)
-            const remainingDays = days % 365 % 30
-            const remainingHours = Math.floor(totalHours % 24)
-            const remainingMinutes = Math.floor((totalHours % 1) * 60)
-
-            let timeString = ''
-            if (years > 0) timeString += `${years}y `
-            if (months > 0) timeString += `${months}M `
-            if (remainingDays > 0) timeString += `${remainingDays}d `
-            if (remainingHours > 0) timeString += `${remainingHours}h `
-            if (remainingMinutes > 0) timeString += `${remainingMinutes}m`
-
-            await context.editReply(`emberglaze has spent \`${hours}\` hours playing HOI4\nThat's approximately ${timeString.trim()}`)
-        } else {
+        if (!hoi4) {
             await context.editReply('❌ HOI4 not found in the list of games (did embi finally touch grass? check his steam profile directly or something)')
+            return
         }
+        const totalHours = hoi4.playtime_forever / 60
+        const hours = totalHours.toFixed(4)
+
+        const days = Math.floor(totalHours / 24)
+        const months = Math.floor(days / 30)
+        const years = Math.floor(days / 365)
+        const remainingDays = days % 365 % 30
+        const remainingHours = Math.floor(totalHours % 24)
+        const remainingMinutes = Math.floor((totalHours % 1) * 60)
+
+        let timeString = ''
+        if (years > 0) timeString += `${years}y `
+        if (months > 0) timeString += `${months}M `
+        if (remainingDays > 0) timeString += `${remainingDays}d `
+        if (remainingHours > 0) timeString += `${remainingHours}h `
+        if (remainingMinutes > 0) timeString += `${remainingMinutes}m`
+
+        await context.editReply(`emberglaze has spent \`${hours}\` hours playing HOI4\nThat's approximately ${timeString.trim()}`)
     }
 } satisfies SlashCommand
