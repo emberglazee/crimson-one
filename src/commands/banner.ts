26c3f443619349899f4774cebbe00a3f98c6d693
emberglaze <emberglaze@emberglaze.ru>
2025-06-06 16:22:23 +0300

    add /banner

---
+++ src/commands/banner.ts
@@ -0,0 +1,33 @@
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import type { SlashCommand } from '../types/types'
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('banner')
+        .setDescription('Show the banner of a user')
+        .addUserOption(uo => uo
+            .setName('user')
+            .setDescription('The user to show the banner of')
+            .setRequired(true)
+        ),
+    async execute(context) {
+        const user = await context.getUserOption('user')
+        if (!user) {
+            await context.reply({ content: 'Please provide a valid user to show the banner of', ephemeral: true })
+            return
+        }
+
+        const banner = user.bannerURL()
+        if (!banner) {
+            await context.reply({ content: 'User does not have a banner', ephemeral: true })
+            return
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor('Random')
+            .setDescription(`[Click here to view the banner](${banner})`)
+            .setImage(banner)
+
+        await context.reply({ embeds: [embed] })
+    }
+} satisfies SlashCommand
