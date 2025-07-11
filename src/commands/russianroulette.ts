d15f4c03a5851ae6cb3620a8d3c5c64dc72007ab
emberglaze <emberglaze@emberglaze.ru>
2025-07-11 18:48:51 +0300

    /roulette: action is no longer required

--- src/commands/russianroulette.ts
+++ src/commands/russianroulette.ts
@@ -8,24 +8,23 @@ export default {
         .addStringOption(so => so
             .setName('action')
             .setDescription('What happens if you lose (e.g., "timeout for 1 minute")')
-            .setRequired(true)
         ),
 
     async execute(context) {
         await context.deferReply()
 
-        const action = context.getStringOption('action', true)
+        const action = context.getStringOption('action')
         const user = await context.getUserOption('user', false, context.author)
         const chamber = Math.floor(Math.random() * 6) + 1
 
         if (chamber === 1) {
             await context.reply({
-                content: `🔫 **BANG!** ${user} Predictable.\nConsequence|| (of power)||: ${action}`,
+                content: `🔫 **BANG!** ${user} Predictable.` + (action ? `\nConsequence|| (of power)||: ${action}` : ''),
                 allowedMentions: { users: [user.id] }
             })
         } else {
             await context.reply({
-                content: `🔫 *click* - ${user} got lucky... Next time.\n-# Specified action was: ${action}`,
+                content: `🔫 *click* - ${user} got lucky... Next time.` + (action ? `\n-# Specified action was: ${action}` : ''),
                 allowedMentions: { users: [user.id] }
             })
         }
